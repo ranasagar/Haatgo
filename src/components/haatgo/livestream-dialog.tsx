e8ed06bad71cf94cc8cdf2edeadb72fd3920e536
Personally Personal <sagarrana@gmail.com>
2025-07-30 07:43:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/haatgo/livestream-dialog.tsx
+++ src/components/haatgo/livestream-dialog.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -22,6 +23,12 @@ export function LivestreamDialog() {
         </Button>
       </DialogTrigger>
       <DialogContent className="max-w-6xl p-0 border-0">
+         <DialogHeader className="sr-only">
+          <DialogTitle>Livestream</DialogTitle>
+          <DialogDescription>
+            Watch the live broadcast and participate in the chat.
+          </DialogDescription>
+        </DialogHeader>
         <div className="grid grid-cols-1 lg:grid-cols-3">
             <div className="lg:col-span-2">
                  <div className="aspect-video bg-muted rounded-tl-lg rounded-tr-lg lg:rounded-tr-none lg:rounded-bl-lg flex items-center justify-center">
