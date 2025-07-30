3ab6f046389e4e34864e3d4a43d92471a75d95f8
Personally Personal <sagarrana@gmail.com>
2025-07-30 18:00:13 +0000

    Error: `DialogContent` requires a `DialogTitle` for the component to be

--- src/components/haatgo/app-header-with-livestream.tsx
+++ src/components/haatgo/app-header-with-livestream.tsx
@@ -29,16 +29,7 @@ export function AppHeaderWithLivestream() {
 
   return (
     <Dialog>
-      <AppHeader
-        livestreamTrigger={
-          <DialogTrigger asChild>
-            <Button className={cn("font-bold transition-all duration-300", !scrolled && "opacity-0 -translate-y-2 pointer-events-none")}>
-              <Radio className="mr-2 h-5 w-5" />
-              Watch Live
-            </Button>
-          </DialogTrigger>
-        }
-      />
+      <AppHeader />
       <DialogContent className="max-w-6xl p-0 border-0 h-[80vh]">
          <DialogHeader className="sr-only">
           <DialogTitle>Livestream</DialogTitle>
