a53279fb02b7475d401556eded09658262d21344
Personally Personal <sagarrana@gmail.com>
2025-07-30 16:43:09 +0000

    There is padding issue at the top of the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,11 @@ export default function Home() {
 
   return (
     <AppShell>
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 lg:py-8">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-6 lg:pt-8">
             <LiveIndicator />
         </div>
         <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col gap-6 lg:gap-8">
+          <div className="flex flex-col gap-6 lg:gap-8 py-6 lg:py-8">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
               <aside className="lg:col-span-1 flex flex-col gap-6 lg:gap-8">
                 <RouteTracker />
