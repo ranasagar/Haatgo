25cb5b0a57aae05dfb46e4a093a0fb78f1258344
Personally Personal <sagarrana@gmail.com>
2025-07-31 04:51:54 +0000

    move 7-Day Forecast below your order status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,10 +28,10 @@ export default function Home() {
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
             <aside className="lg:col-span-1 flex flex-col gap-6 lg:gap-8">
               <RouteTracker />
-              <WeatherForecast />
               <SendPackage />
               <MyOrders />
               <OrderTracker />
+              <WeatherForecast />
             </aside>
             <div className="lg:col-span-2 flex flex-col gap-6 lg:gap-8">
               <Recommendations allProducts={products} />
